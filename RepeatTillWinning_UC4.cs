0e7ee636b8a5ff1140debd59a0d84fc5173b23d7
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-22 23:07:13 +0530

    Repeat till the player till reaches 100

---
+++ RepeatTillWinning_UC4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class RepeatTillWinning_UC4
+    {
+        public static void RepeatTheDiceTillWinning()
+        {
+            int Start = 0;
+            const int noPlay = 1;
+            const int snake = 2;
+            const int ladder = 3;
+            int position = 0;
+            int dice, option;
+
+            Random random = new Random();
+
+            while (position <= 100)
+            {
+                dice = random.Next(1, 7);
+                option = random.Next(1, 4);
+                if (position < 0)
+                {
+                    position = 0;
+                }
+                switch (option)
+                {
+                    case noPlay:
+                        position += 0;
+                        break;
+                    case snake:
+                        position -= dice;
+                        break;
+                    case ladder:
+                        position += dice;
+                        break;
+                }
+                Console.WriteLine(position);
+            }
+        }
+    }
+}
