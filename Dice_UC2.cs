e7d2205c6e2b3192dc56643a7c3bfc64d33217b3
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-22 23:01:30 +0530

    when dice roll

---
+++ Dice_UC2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class Dice_UC2
+    {
+        public static void FindingDiceValue()
+        {
+            int initialValue = 0, FinalValue = 0;
+            
+            Random random = new Random();
+            int number = random.Next(1, 7);
+
+            Console.WriteLine(number);
+        }
+    }
+}
