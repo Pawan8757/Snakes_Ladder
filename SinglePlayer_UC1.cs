879aba4b7e9d35e47ff88c3cd3a889798a034089
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-22 22:59:15 +0530

    snakes and ladder game played by single player

---
+++ SinglePlayer_UC1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class SinglePlayer_UC1
+    {
+        public static void PlayingSinglePlayer()
+        {
+            int Start = 0;
+            int Position = 0;
+            Console.WriteLine(Position);
+        }
+    }
+}
+           
