5222aaf5eeb65450c933df535a99333a0a7ddd32
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-22 22:54:41 +0530

    adding welcome program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SnakeLadder
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Solution Of Snakes & Ladder Problem");
+            //SinglePlayer_UC1.PlayingSinglePlayer();
+            //Dice_UC2.FindingDiceValue();
+            //CheckingForPlay_UC3.CheckingForPlay();
+            //RepeatTillWinning_UC4.RepeatTheDiceTillWinning();
+            //EnsureThePlayerGetTrophy_UC5.EnsurityOfWinning();
+           ReportTheDice_UC6.ReportingAboutDice();
+
+
+        }
+    }
+}
